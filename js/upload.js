c78c5fae66f025e697dfe0652f547cc0dd6ee732
Stanislau <savandex@gmail.com>
2020-02-03 15:53:16 -0800

    upload form to rel server

---
+++ js/upload.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function() {
+  const URL = "https://js.dump.academy/kekstagram";
+  window.upload = function(data, onSuccess) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.addEventListener("load", function() {
+      switch(xhr.status) {
+          case 200: 
+            onSuccess(xhr.response);
+            break;
+          case 500:
+              console.error('Wrong address');
+      }
+    });
+
+    xhr.open("POST", URL);
+    xhr.send(data);
+  };
+})();
